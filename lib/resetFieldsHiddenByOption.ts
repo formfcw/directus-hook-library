df3ee41479aba9d02c3b10de15102c0e69715752
formfcw <dev@formfcw.com>
2024-02-03 21:32:31 +0100

    reset fields only if optionsField is present in input at resetFieldsHiddenByOption

--- lib/resetFieldsHiddenByOption.ts
+++ lib/resetFieldsHiddenByOption.ts
@@ -1,6 +1,5 @@
 import type { RegisterFunctions } from "./types";
 import type { HookExtensionContext } from "@directus/extensions";
-import type { EventContext } from "@directus/types";
 
 type ResetGroup = {
     not: string[];
@@ -15,39 +14,16 @@ type ResetContext = {
 
 export default function (
     { filter }: RegisterFunctions,
-    { services, getSchema }: HookExtensionContext,
+    context: HookExtensionContext,
     { collection, optionsField, resetGroups }: ResetContext
 ) {
-    const { ItemsService } = services;
-
     filter(`${collection}.items.create`, handler);
     filter(`${collection}.items.update`, handler);
 
-    async function handler(
-        input: any,
-        meta: Record<string, any>,
-        context: EventContext
-    ) {
+    async function handler(input: any) {
         if (input.hasOwnProperty(optionsField)) {
             _resetBySelectedOption(input[optionsField]);
             return input;
-        }
-
-        if (!meta.keys) return input;
-
-        const itemsService = new ItemsService(meta.collection, {
-            ...context,
-            schema: await getSchema(),
-        });
-
-        for (const key of meta.keys) {
-            const item = await itemsService.readOne(key, {
-                fields: [optionsField],
-            });
-
-            if (item?.[optionsField]) {
-                _resetBySelectedOption(item[optionsField]);
-            }
         }
 
         return input;
